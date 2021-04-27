f2678fae579dbf579974668e0992fe5eee2dc8ad
Abdul Aziz Ahwan <abdulazizahwan97@gmail.com>
2021-04-27 09:31:46 +0700

    remove host

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: process.env.PORT,
-        host: process.env.HOST,
+        port: process.env.PORT || 5000,
         routes: {
             cors: {
                 origin: ['*'],
